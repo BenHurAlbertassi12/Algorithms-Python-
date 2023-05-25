057b80786e314dc08171d6c7b2d0617d2f3103f6
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-25 15:49:06 -0300

    is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if low_index >= high_index:
+        return True
+    # Retorne True se a palavra passada por parâmetro for um palíndromo;
+
+    if word[low_index] != word[high_index]:
+        return False
+    # Retorne False se a palavra passada por parâmetro não for um palíndromo;
+
+    if len(word) == 0:
+        return False
+    #  Retorne False se nenhuma palavra for passada por parâmetro
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
