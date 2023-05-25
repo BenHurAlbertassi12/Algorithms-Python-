f7b1a673e8ac74b14ffaa8dffe42b20e5219c55a
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-25 19:14:19 -0300

    test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('Tom Marvolo Riddle', '5')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 5)
+
+    assert encrypt_message('Tom Marvolo Riddle', 10) == 'i am lord voldemort'
+    assert encrypt_message('Tom Marvolo Riddle', 5) == 'i am lord_voldemort'
